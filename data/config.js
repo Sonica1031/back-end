c7d277b435aac28c5cbda718663f20aaa3a29470
Sonica1031 <Sonica1031@hotmail.com>
2020-07-27 18:38:28 -0500

    hopeful

--- data/config.js
+++ data/config.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const knexfile = require("../knexfile");
 
-const environment = process.env.NODE_ENV;
+const environment = 'development'
 
 module.exports = knex(knexfile[environment]);
