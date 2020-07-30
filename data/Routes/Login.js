4e21774dcf5f6a872ed00342fb0a24b1cbfec8f4
Sonica1031 <Sonica1031@hotmail.com>
2020-07-29 19:00:03 -0500

    working token

--- data/Routes/Login.js
+++ data/Routes/Login.js
@@ -2,20 +2,36 @@ const express = require("express");
 const LM = require("./Login-model.js");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+const secret = require('../secret');
 
 router.post("/", async (req, res, next) => {
     try{
     const {username, password} = req.body;
     const user = await LM.Find(username);
+    const token = jas(user);
+    console.log (token)
     if(!user || !bcrypt.compareSync(password, user[0].password)){
         res.status(500).json({errorMessage: "invalid user!"});
     }else{
         req.session.name = user[0].username;
-        res.json({user});
+        res.json({user, token});
     }
 }catch(error){
     next(error);
 }
 })
+
+const jas = (user) => {
+    const payload = {
+        subject: user.id,
+        username: user.username,
+    };
+    const options = {
+        expiresIn: '1d',
+    };
+
+    return jwt.sign(payload, secret.jwtSecret, options);
+}
 
 module.exports = router;
