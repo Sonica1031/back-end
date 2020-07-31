1e1c592a1dcc3e3d965820f47c2b1e79a5441ccb
Sonica1031 <Sonica1031@hotmail.com>
2020-07-30 20:05:43 -0500

    made some tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,5 +1,6 @@
 const supertest = require("supertest");
 const server = require("../index");
+const db = require("../data/config");
 
 test("GET /", async () => {
     const res = await supertest(server).get("/api/items")
@@ -15,5 +16,21 @@ test("REGISTER /", async () => {
 })
 
 test("LOGIN /", async () => {
-    const res = await supertest(server).post("")
+    const res = await supertest(server).post("/api/login")
+    .send({username: "Testing_Something", password: "Testing_Something"})
+    .expect(200)
+    .expect('Content-Type', 'application/json; charset=utf-8');
+})
+
+test("ITEMS /", async () => {
+    const res = await supertest(server).get("/api/items")
+    .expect(200)
+    .expect('Content-Type', 'application/json; charset=utf-8');
+})
+
+test("POST ITEMS /", async () => {
+    const res = await supertest(server).post("/api/items")
+    .send({itemName : "carrot", itemType : 1, price : 2.00, itemdesc : "Crunchy and Juicy", imageUrl: "test.com", itemLocation : "Tanzania"})
+    .expect(201)
+    .expect('Content-Type', 'application/json; charset=utf-8');
 })
