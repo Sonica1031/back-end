493629b66b183c97b536e67403f424bd5a588485
Sonica1031 <Sonica1031@hotmail.com>
2021-04-11 21:33:23 -0500

    Fixed the cart and edible tables and functions

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const server = express();
-
 const cors = require("cors");
+const server = express();
 const helmet = require("helmet");
 const session = require('express-session');
 const protect = require("./data/middleware/authenticate-middleware");
